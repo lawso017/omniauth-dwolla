695d4f14e42bc12072665eca7905c1675d884346
Stefano Bernardi <hello@stefanobernardi.com>
2013-04-30 21:10:13 -0700

    Dwolla APIs are even more stupid.

--- lib/omniauth/strategies/dwolla.rb
+++ lib/omniauth/strategies/dwolla.rb
@@ -11,17 +11,17 @@ module OmniAuth
         :token_url => '/oauth/v2/token'
       }
 
-      uid { raw_info['Id'] }
+      uid { raw_info['Response']['Id'] }
 
       info do
         {
-          :name => raw_info['Name'],
+          :name => raw_info['Response']['Name'],
         }
       end
       
       extra do
         {
-          :raw_info => raw_info
+          :raw_info => raw_info['Response']
         }
       end
 
@@ -34,9 +34,7 @@ module OmniAuth
       def raw_info
         access_token.options[:mode] = :query
         access_token.options[:param_name] = :oauth_token
-        @raw_info ||= MultiJson.load(access_token.get('https://www.dwolla.com/oauth/rest/users/').response)
-        Rails.logger.info @raw_info
-        @raw_info
+        @raw_info ||= MultiJson.load(access_token.get('https://www.dwolla.com/oauth/rest/users/').body)
       end
 
      end
